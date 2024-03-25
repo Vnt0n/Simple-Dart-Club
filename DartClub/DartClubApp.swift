9b6d859950e08957bd1a53ea68425d5613788a1d
Vnt0n <antoine.chosson@gmail.com>
2024-03-25 14:53:01 +0100

    Initial Commit

---
+++ DartClub/DartClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartClubApp.swift
+//  DartClub
+//
+//  Created by Antoine on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DartClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
