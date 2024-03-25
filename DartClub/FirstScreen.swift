60857ae06b14392fc093982061b8dfdb63662bfd
Vnt0n <antoine.chosson@gmail.com>
2024-03-25 18:27:50 +0100

    First screen added

---
+++ DartClub/FirstScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FirstScreen.swift
+//  DartClub
+//
+//  Created by Antoine on 25/03/2024.
+//
+
+import SwiftUI
+
+struct FirstScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstScreen()
+}
