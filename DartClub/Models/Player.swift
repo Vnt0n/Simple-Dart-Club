2c65cdebcb8951904502edc4fc30e9b192eb4fa8
Vnt0n <antoine.chosson@gmail.com>
2024-03-27 15:58:03 +0100

    Player model created

---
+++ DartClub/Models/Player.swift
@@ -0,0 +1,21 @@
+//
+//  Player.swift
+//  DartClub
+//
+//  Created by Antoine on 27/03/2024.
+//
+
+import Foundation
+import SwiftData
+
+class Player {
+    let name: String
+    let score: Int
+    
+    init(name: String, score: Int){
+        self.name = name
+        self.score = score
+    }
+}
+
+
