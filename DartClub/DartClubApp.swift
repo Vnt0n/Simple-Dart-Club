10aee0fe79a65b834b0eeb66d918b846b5851d37
Vnt0n <antoine.chosson@gmail.com>
2024-03-28 18:59:25 +0100

    hide Status Bar in App

--- DartClub/DartClubApp.swift
+++ DartClub/DartClubApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Dart_ClubApp: App {
     var body: some Scene {
         WindowGroup {
-            LaunchView()
+            LaunchView().statusBar(hidden: true)
         }
     }
 }
