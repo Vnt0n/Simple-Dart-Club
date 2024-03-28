cadc1afec24b31ea242c6726e374a9862a540f2f
Vnt0n <antoine.chosson@gmail.com>
2024-03-28 14:38:53 +0100

    fix ignore safe area when 2 players only

--- DartClub/Views/GameView.swift
+++ DartClub/Views/GameView.swift
@@ -32,6 +32,7 @@ struct GameView: View {
             ZStack {
                 Color.gray
                     .frame(maxWidth: .infinity)
+                    .edgesIgnoringSafeArea(.bottom)
                 VStack {
                     Text(name2)
                     Text("501")
