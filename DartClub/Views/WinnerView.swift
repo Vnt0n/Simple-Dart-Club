429f8ba7d424bedccc61865ddca96c1329477e1d
Vnt0n <antoine.chosson@gmail.com>
2024-03-29 00:14:07 +0100

    Added a WinnerView

---
+++ DartClub/Views/WinnerView.swift
@@ -0,0 +1,32 @@
+//
+//  Contentview.swift
+//  DartClub
+//
+//  Created by Antoine on 29/03/2024.
+//
+
+import SwiftUI
+import ConfettiSwiftUI
+
+struct WinnerView: View {
+    
+    @State private var counter = 0
+    
+    var body: some View {
+        Button(action: {
+            counter += 1
+        }) {
+            Text("You won!")
+                .font(.system(size: 50))
+        }
+        .confettiCannon(counter: $counter, num: 100, radius: 500.0)
+        .onAppear {
+            // Simuler un clic sur le bouton
+            self.counter += 1
+        }
+    }
+}
+
+#Preview {
+    WinnerView()
+}
