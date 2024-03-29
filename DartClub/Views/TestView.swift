f0300472dfbf412fc59e6987a017396f0ba2fe12
Vnt0n <antoine.chosson@gmail.com>
2024-03-29 09:07:15 +0100

    New test var on WinnerView and testViewAdded

---
+++ DartClub/Views/TestView.swift
@@ -0,0 +1,22 @@
+//
+//  TestView.swift
+//  DartClub
+//
+//  Created by Antoine on 29/03/2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+        })
+        
+    }
+}
+
+#Preview {
+    TestView()
+}
--- DartClub/Views/WinnerView.swift
+++ DartClub/Views/WinnerView.swift
@@ -11,23 +11,27 @@ import ConfettiSwiftUI
 struct WinnerView: View {
     
     @State private var counter = 0
+    var namePlayer1: String
     
     var body: some View {
-        Button(action: {
-            counter += 1
-        }) {
-            Text("You won!")
-                .font(.system(size: 50))
-        }
-        .confettiCannon(counter: $counter, num: 100, radius: 500.0)
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-                self.counter += 1
+        VStack {
+            Text(namePlayer1)
+            Button(action: {
+                counter += 1
+            }) {
+                Text("You won!")
+                    .font(.system(size: 50))
+            }
+            .confettiCannon(counter: $counter, num: 100, radius: 500.0)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                    self.counter += 1
+                }
             }
         }
     }
 }
 
 #Preview {
-    WinnerView()
+    WinnerView(namePlayer1: "namePlayer1")
 }
