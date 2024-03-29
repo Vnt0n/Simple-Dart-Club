0bbe2ba67b00696b3cde85fea3078a38492f3f9e
Vnt0n <antoine.chosson@gmail.com>
2024-03-29 09:10:33 +0100

    TestView Button

--- DartClub/Views/TestView.swift
+++ DartClub/Views/TestView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 struct TestView: View {
     var body: some View {
         
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-        })
+        Button("OK          ") {
+            
+        }
+        .buttonStyle(.borderedProminent)
+        .controlSize(.large)
+        .padding()
         
     }
 }
