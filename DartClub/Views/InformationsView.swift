a7ce13976a1a47e04a56804fc24f587ccdff76eb
Vnt0n <antoine.chosson@gmail.com>
2024-04-04 22:56:39 +0200

    Some first front on InformationsView

--- DartClub/Views/InformationsView.swift
+++ DartClub/Views/InformationsView.swift
@@ -20,15 +20,26 @@ struct InformationsView: View {
     
     var body: some View {
         VStack {
-            Text("Score Histories:")
+            
+            Text("Score Histories")
                 .font(.title)
                 .padding()
-
+            
             ScrollView {
+            
+                VStack {
+                    Text("Actual Score")
+                        .font(.headline)
+                    Text("Player 1: \(player1Scores)")
+                    Text("Player 2: \(player2Scores)")
+                    Text("Player 3: \(player3Scores)")
+                }
+                .padding()
+            
                 VStack {
                     ForEach(scoreHistories, id: \.self) { history in
                         VStack {
-                            Text("Game \(scoreHistories.firstIndex(of: history)! + 1):")
+                            Text("GAME \(scoreHistories.firstIndex(of: history)! + 1)")
                                 .font(.headline)
                             
                             Text("Player 1: \(history.player1)")
@@ -38,18 +49,16 @@ struct InformationsView: View {
                         .padding()
                     }
                 }
+                
                 VStack {
-                                Text("Player 1: \(scorePlayer1)")
-                                Text("Player 2: \(scorePlayer2)")
-                                Text("Player 3: \(scorePlayer3)")
-                            }
-                            .padding()
-                VStack {
-                                    Text("Player 1: \(player1Scores)")
-                                    Text("Player 2: \(player2Scores)")
-                                    Text("Player 3: \(player3Scores)")
-                                }
-                                .padding()
+                    Text("Actual Score")
+                        .font(.headline)
+                    Text("Player 1: \(scorePlayer1)")
+                    Text("Player 2: \(scorePlayer2)")
+                    Text("Player 3: \(scorePlayer3)")
+                }
+                .padding()
+
             }
         }
     }
@@ -60,14 +69,14 @@ struct InformationsView_Previews: PreviewProvider {
     
     static var previews: some View {
         let scoreHistories: [GameView.ScoreHistory] = [
-            GameView.ScoreHistory(player1: [100, 200, 300], player2: [150, 250, 350], player3: [180, 220, 320]),
+            GameView.ScoreHistory(player1: [5, 57, 25, 65, 89, 45, 78], player2: [45, 36, 75, 29, 27, 65, 25], player3: [14, 28, 35, 45, 20, 18, 43]),
             GameView.ScoreHistory(player1: [120, 220, 320], player2: [130, 230, 330], player3: [160, 240, 340])
         ]
         
         return InformationsView(scoreHistories: .constant(scoreHistories),
-                                player1Scores: .constant([500, 450, 400]),
-                                player2Scores: .constant([510, 460, 410]),
-                                player3Scores: .constant([520, 470, 420]),
+                                player1Scores: .constant([10, 55, 35]),
+                                player2Scores: .constant([25, 41, 17]),
+                                player3Scores: .constant([54, 22, 14]),
                                 scorePlayer1: 490,
                                 scorePlayer2: 480,
                                 scorePlayer3: 470)
