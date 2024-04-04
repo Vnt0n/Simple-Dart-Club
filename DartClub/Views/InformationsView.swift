5de78012ea6916fa84942bc683045406550c13ec
Vnt0n <antoine.chosson@gmail.com>
2024-04-05 00:16:26 +0200

    Fix Preview InformationsView

--- DartClub/Views/InformationsView.swift
+++ DartClub/Views/InformationsView.swift
@@ -198,23 +198,22 @@ struct InformationsView: View {
             }
         }
     }
+}
+
+struct InformationsView_Previews: PreviewProvider {
     
-    
-    struct InformationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        let scoreHistories: [GameView.ScoreHistory] = [
+            GameView.ScoreHistory(player1: [5, 57, 25, 65, 89, 45, 78], player2: [45, 36, 75, 29, 27, 65, 25], player3: [14, 28, 35, 45, 20, 18, 43]),
+            GameView.ScoreHistory(player1: [120, 220, 320], player2: [130, 230, 330], player3: [160, 240, 340])
+        ]
         
-        static var previews: some View {
-            let scoreHistories: [GameView.ScoreHistory] = [
-                GameView.ScoreHistory(player1: [5, 57, 25, 65, 89, 45, 78], player2: [45, 36, 75, 29, 27, 65, 25], player3: [14, 28, 35, 45, 20, 18, 43]),
-                GameView.ScoreHistory(player1: [120, 220, 320], player2: [130, 230, 330], player3: [160, 240, 340])
-            ]
-            
-            return InformationsView(scoreHistories: .constant(scoreHistories),
-                                    player1Scores: .constant([10, 55, 35]),
-                                    player2Scores: .constant([25, 41, 17]),
-                                    player3Scores: .constant([54, 22, 14]),
-                                    scorePlayer1: 390,
-                                    scorePlayer2: 180,
-                                    scorePlayer3: 250)
-        }
+        return InformationsView(scoreHistories: .constant(scoreHistories),
+                                player1Scores: .constant([10, 55, 35]),
+                                player2Scores: .constant([25, 41, 17]),
+                                player3Scores: .constant([54, 22, 14]),
+                                scorePlayer1: 390,
+                                scorePlayer2: 180,
+                                scorePlayer3: 250)
     }
 }
