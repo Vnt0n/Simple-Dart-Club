2530d48bc515b88c15a4c2ba789225d60a77d075
Vnt0n <antoine.chosson@gmail.com>
2024-04-04 19:16:38 +0200

    Ok so far with previews

--- DartClub/Views/InformationsView.swift
+++ DartClub/Views/InformationsView.swift
@@ -54,3 +54,22 @@ struct InformationsView: View {
         }
     }
 }
+
+
+struct InformationsView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        let scoreHistories: [GameView.ScoreHistory] = [
+            GameView.ScoreHistory(player1: [100, 200, 300], player2: [150, 250, 350], player3: [180, 220, 320]),
+            GameView.ScoreHistory(player1: [120, 220, 320], player2: [130, 230, 330], player3: [160, 240, 340])
+        ]
+        
+        return InformationsView(scoreHistories: .constant(scoreHistories),
+                                player1Scores: .constant([500, 450, 400]),
+                                player2Scores: .constant([510, 460, 410]),
+                                player3Scores: .constant([520, 470, 420]),
+                                scorePlayer1: 490,
+                                scorePlayer2: 480,
+                                scorePlayer3: 470)
+    }
+}
