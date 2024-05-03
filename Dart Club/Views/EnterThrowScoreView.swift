d8176d5bb19a59514b743e3ac8137e5c49f0f28d
Vnt0n <antoine.chosson@gmail.com>
2024-05-03 14:29:21 +0200

    EnterScoreView: all logic exceok

--- Dart Club/Views/EnterThrowScoreView.swift
+++ Dart Club/Views/EnterThrowScoreView.swift
@@ -8,29 +8,13 @@
 import SwiftUI
 
 struct EnterThrowScoreView: View {
-    
     @ObservedObject var viewModel: GameViewModel
-    
     @Binding var currentPlayerIndex: Int
-
-    @State private var firstThrowScore: Int?
-    @State private var secondThrowScore: Int?
-    @State private var thirdThrowScore: Int?
-    @State private var isScoreValidated1 = false
-    @State private var isScoreValidated2 = false
-    @State private var isScoreValidated3 = false
-    @State private var isDouble = false
-    @State private var isDouble1 = false
-    @State private var isDouble2 = false
-    @State private var isDouble3 = false
-    @State private var isTriple = false
-    @State private var isTriple1 = false
-    @State private var isTriple2 = false
-    @State private var isTriple3 = false
-    @State private var isUndo = false
+    @State private var throwScores = [Int?](repeating: nil, count: 3)
+    @State private var isDouble = [false, false, false]
+    @State private var isTriple = [false, false, false]
 
     @FocusState private var isFocused: Bool
-    
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
@@ -46,271 +30,122 @@ struct EnterThrowScoreView: View {
                     .padding(.bottom, 1)
 
                 Text("Enter your score")
-                    .font(.system(size: 20,  design: .default))
-                
-                HStack {
-                    
-                    Button(action: {
-                        if let score = firstThrowScore {
-                            if isTriple1 {
+                    .font(.system(size: 20, design: .default))
 
-                                firstThrowScore = score / 3
-                                isTriple1 = false
-                            } else {
-
-                                firstThrowScore = score * 3
-                                isTriple1 = true
-                            }
-                        }
-                    }) {
-                        Image(systemName: isTriple1 ? "3.square.fill" : "3.square")
-                            .accessibilityLabel("Toggle Triple")
-                            .font(.system(size: 25))
-                    }
-
-                    Button(action: {
-                        if let score = firstThrowScore {
-                            if isDouble1 {
-
-                                firstThrowScore = score / 2
-                                isDouble1 = false
-                            } else {
-
-                                firstThrowScore = score * 2
-                                isDouble1 = true
-                            }
-                        }
-                    }) {
-                        Image(systemName: isDouble1 ? "2.square.fill" : "2.square")
-                            .accessibilityLabel("Toggle Triple")
-                            .font(.system(size: 25))
-                    }
-                    
-                    TextField("1st throw", value: $firstThrowScore, format: .number)
-                        .font(.system(size: 23, design: .default))
-                        .multilineTextAlignment(.center)
-                        .padding()
-                        .keyboardType(.decimalPad)
-                        .frame(width: 150)
-                        .focused($isFocused)
-                        .onAppear {
-                            self.isFocused = true
-                        }
-                        .onSubmit {
-                            
-                        }
-
-                    Button(action: {
-                        isUndo = true
-                    }) {
-                        Image(systemName: "arrow.uturn.backward.circle")
-                            .accessibilityLabel("Undo")
-                            .font(.system(size: 25))
-                    }
-
-                    if isScoreValidated1 {
-                        Button(action: {
-                            isScoreValidated1 = true
-                        }) {
-                            Image(systemName: "checkmark.circle.fill")
-                                .accessibilityLabel("Undo")
-                                .font(.system(size: 25))
-                        }
-                    } else {
-                        Button(action: {
-                            isScoreValidated1 = true
-                        }) {
-                            Image(systemName: "circle")
-                                .accessibilityLabel("Undo")
-                                .font(.system(size: 25))
-                        }
-                    }
-                    
-                }
-                
-                HStack {
-                    
-                    Button(action: {
-                        if let score = secondThrowScore {
-                            if isTriple2 {
-
-                                secondThrowScore = score / 3
-                                isTriple2 = false
-                            } else {
-
-                                secondThrowScore = score * 3
-                                isTriple2 = true
-                            }
-                        }
-                    }) {
-                        Image(systemName: isTriple2 ? "3.square.fill" : "3.square")
-                            .accessibilityLabel("Toggle Triple")
-                            .font(.system(size: 25))
-                    }
-
-                    Button(action: {
-                        if let score = secondThrowScore {
-                            if isDouble2 {
-
-                                secondThrowScore = score / 2
-                                isDouble2 = false
-                            } else {
-
-                                secondThrowScore = score * 2
-                                isDouble2 = true
-                            }
-                        }
-                    }) {
-                        Image(systemName: isDouble2 ? "2.square.fill" : "2.square")
-                            .accessibilityLabel("Toggle Triple")
-                            .font(.system(size: 25))
-                    }
-
-                    TextField("2nd throw", value: $secondThrowScore, format: .number)
-                        .font(.system(size: 23, design: .default))
-                        .multilineTextAlignment(.center)
-                        .padding()
-                        .keyboardType(.decimalPad)
-                        .frame(width: 150)
-                    
-                    Button(action: {
-                        isUndo = true
-                    }) {
-                        Image(systemName: "arrow.uturn.backward.circle")
-                            .accessibilityLabel("Undo")
-                            .font(.system(size: 25))
-                    }
-                    
-                    if isScoreValidated2 {
-                        Button(action: {
-                            isScoreValidated2 = true
-                        }) {
-                            Image(systemName: "checkmark.circle.fill")
-                                .accessibilityLabel("Undo")
-                                .font(.system(size: 25))
-                        }
-                    } else {
-                        Button(action: {
-                            isScoreValidated2 = true
-                        }) {
-                            Image(systemName: "circle")
-                                .accessibilityLabel("Undo")
-                                .font(.system(size: 25))
-                        }
-                    }
-                    
+                ForEach(0..<3) { index in
+                    ScoreInputRow(index: index, score: $throwScores[index], isDouble: $isDouble[index], isTriple: $isTriple[index])
                 }
 
-                HStack {
-                    
-                    Button(action: {
-                        if let score = thirdThrowScore {
-                            if isTriple3 {
-
-                                thirdThrowScore = score / 3
-                                isTriple3 = false
-                            } else {
-
-                                thirdThrowScore = score * 3
-                                isTriple3 = true
-                            }
-                        }
-                    }) {
-                        Image(systemName: isTriple3 ? "3.square.fill" : "3.square")
-                            .accessibilityLabel("Toggle Triple")
-                            .font(.system(size: 25))
-                    }
-
-                    Button(action: {
-                        if let score = thirdThrowScore {
-                            if isDouble3 {
-
-                                thirdThrowScore = score / 2
-                                isDouble3 = false
-                            } else {
-
-                                thirdThrowScore = score * 2
-                                isDouble3 = true
-                            }
-                        }
-                    }) {
-                        Image(systemName: isDouble3 ? "2.square.fill" : "2.square")
-                            .accessibilityLabel("Toggle Triple")
-                            .font(.system(size: 25))
-                    }
-                    
-                    TextField("3rd throw", value: $thirdThrowScore, format: .number)
-                        .font(.system(size: 23, design: .default))
-                        .multilineTextAlignment(.center)
-                        .padding()
-                        .keyboardType(.decimalPad)
-                        .frame(width: 150)
-                    
-                    Button(action: {
-                        isUndo = true
-                    }) {
-                        Image(systemName: "arrow.uturn.backward.circle")
-                            .accessibilityLabel("Undo")
-                            .font(.system(size: 25))
-                    }
-                    
-                    if isScoreValidated3 {
-                        Button(action: {
-                            isScoreValidated3 = true
-                        }) {
-                            Image(systemName: "checkmark.circle.fill")
-                                .accessibilityLabel("Undo")
-                                .font(.system(size: 25))
-                        }
-                    } else {
-                        Button(action: {
-                            isScoreValidated3 = true
-                        }) {
-                            Image(systemName: "circle")
-                                .accessibilityLabel("Undo")
-                                .font(.system(size: 25))
-                        }
-                    }
-                    
-                }
-                
                 Button("OK          ") {
-                    
-                    if let first = firstThrowScore, let second = secondThrowScore, let third = thirdThrowScore {
-                        
-                        let scores = [first, second, third]
-                        viewModel.addScore(forPlayer: currentPlayerIndex, score: scores)
-                        currentPlayerIndex = (currentPlayerIndex + 1) % viewModel.currentGame.players.count
-                        dismiss()
-                        
-                    }
-                    
+                    submitScores()
                 }
                 .buttonStyle(.borderedProminent)
                 .controlSize(.large)
                 .padding()
-                .disabled(!allFieldsHaveValues)
-                
-                Spacer()
+                .disabled(!allScoresEntered)
 
+                Spacer()
             }
         }
     }
 
-    var allFieldsHaveValues: Bool {
-        let scores = [firstThrowScore, secondThrowScore, thirdThrowScore].compactMap { $0 }
-        return scores.count == 3 && scores.allSatisfy { $0 <= 60 }
+    var allScoresEntered: Bool {
+        throwScores.allSatisfy { $0 != nil }
     }
-    
-    func doubleScore(initialScore: Int?) -> Int {
-        if var score = initialScore {
-            score *= 2
-            return score
+
+    func submitScores() {
+        if allScoresEntered, let first = throwScores[0], let second = throwScores[1], let third = throwScores[2] {
+            let scores = [first, second, third]
+            viewModel.addScore(forPlayer: currentPlayerIndex, score: scores)
+            currentPlayerIndex = (currentPlayerIndex + 1) % viewModel.currentGame.players.count
+            dismiss()
         }
-        return 0
     }
-    
 }
+
+struct ScoreInputRow: View {
+    let index: Int
+    @Binding var score: Int?
+    @Binding var isDouble: Bool
+    @Binding var isTriple: Bool
+
+    var body: some View {
+        HStack {
+            
+            Spacer()
+            
+            ToggleScoreButton(systemImageName: "2.square", isActivated: $isDouble, otherIsActivated: $isTriple, score: $score, factor: 2)
+            ToggleScoreButton(systemImageName: "3.square", isActivated: $isTriple, otherIsActivated: $isDouble, score: $score, factor: 3)
+
+            TextField("\(index+1)st throw", value: $score, format: .number)
+                .font(.system(size: 23))
+                .multilineTextAlignment(.center)
+                .padding()
+                .keyboardType(.decimalPad)
+                .frame(width: 130)
+                .onChange(of: score) {
+                    if score == nil {
+                        isDouble = false
+                        isTriple = false
+                    }
+                }
+            
+            Spacer()
+            Spacer()
+
+        }
+    }
+}
+
+struct ToggleScoreButton: View {
+    let systemImageName: String
+    @Binding var isActivated: Bool
+    @Binding var otherIsActivated: Bool
+    @Binding var score: Int?
+    let factor: Int
+
+    var body: some View {
+        Button(action: toggleState) {
+            Image(systemName: iconForButton())
+                .font(.system(size: 25))
+        }
+        .foregroundColor(colorForButton())
+        .disabled(shouldDisableButton() || otherIsActivated || score == nil)  // Disable if any condition applies
+    }
+
+    private func toggleState() {
+        guard let currentScore = score, !(currentScore == 25 || currentScore == 50) else { return }
+        score = isActivated ? currentScore / factor : currentScore * factor
+        isActivated.toggle()
+        if isActivated {
+            otherIsActivated = false
+        }
+    }
+
+    private func shouldDisableButton() -> Bool {
+        guard let score = score else { return true }
+        return score == 25 || score == 50 || !(score <= 50 || (score > 20 && score < 50))
+    }
+
+    private func iconForButton() -> String {
+        // Return the non-fill icon if the button is disabled
+        if shouldDisableButton() || otherIsActivated || score == nil {
+            return systemImageName
+        } else {
+            return isActivated ? "\(systemImageName).fill" : systemImageName
+        }
+    }
+
+    private func colorForButton() -> Color {
+        // Always gray when disabled, blue when active or activable
+        if shouldDisableButton() || otherIsActivated || score == nil {
+            return .gray
+        } else {
+            return .blue
+        }
+    }
+}
+
 
 
 // ///////////////////////////
