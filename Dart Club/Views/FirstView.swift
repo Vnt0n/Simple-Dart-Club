0066086ba774368f2f4ef2b6f02d700f6eed01ea
Vnt0n <antoine.chosson@gmail.com>
2024-05-07 22:31:52 +0200

    iPad FirstView OK

--- Dart Club/Views/FirstView.swift
+++ Dart Club/Views/FirstView.swift
@@ -12,10 +12,8 @@ class DeviceOrientationManager: ObservableObject {
     @Published var isLandscape: Bool = false
     @Published var isFlat: Bool = false
 
-
     init() {
         NotificationCenter.default.addObserver(self, selector: #selector(orientationChanged), name: UIDevice.orientationDidChangeNotification, object: nil)
-        
         UIDevice.current.beginGeneratingDeviceOrientationNotifications()
     }
 
@@ -28,17 +26,13 @@ class DeviceOrientationManager: ObservableObject {
         DispatchQueue.main.async {
             self.isLandscape = UIDevice.current.orientation.isLandscape
             self.isFlat = UIDevice.current.orientation.isFlat
-
         }
     }
 }
 
 struct FirstView: View {
-    
     @ObservedObject private var orientationDetected = DeviceOrientationManager.shared
-
     @AppStorage("isDarkMode") var isDarkMode: Bool = false
-
     @State private var navigateToGame = false
     @StateObject var viewModel = GameViewModel(gameType: 0)
     @FocusState private var focusedPlayerIndex: Int?
@@ -46,273 +40,192 @@ struct FirstView: View {
     @State private var isDartClubVisible = true
 
     var body: some View {
-        
-        NavigationStack {
-            
-            ZStack {
-                
-                Color.black.ignoresSafeArea(.all)
-
-                VStack {
-
-                    Spacer()
+        GeometryReader { geometry in
+            NavigationStack {
+                ZStack {
+                    Color.black.ignoresSafeArea(.all)
                     
-                    if UIDevice.current.userInterfaceIdiom == .pad && orientationDetected.isLandscape || UIDevice.current.userInterfaceIdiom == .pad && UIDevice.current.orientation.isLandscape {
+                    if UIDevice.current.userInterfaceIdiom == .pad {
                         
-                        HStack {
-                                                        
-                            dartClubText
-                                .frame(maxWidth: 650)
-                            
-                            VStack {
-                                
+                        if geometry.size.width > 1050 {
+                            HStack {
                                 Spacer()
-                                
-                                playerFields
-                                                                
-                                if viewModel.currentGame.players.count < 4 {
-                                    Button(action: {
-                                        withAnimation {
-                                            viewModel.addPlayer()
-                                            focusedPlayerIndex = viewModel.currentGame.players.count - 1
-                                        }
-                                    }) {
-                                        Label("Add a player", systemImage: "person.fill.badge.plus")
-                                            .accessibilityLabel("Add a player")
-                                            .font(.system(size: 20))
-                                            .padding(.top, 20)
-                                    }
-                                    .buttonStyle(PlainButtonStyle())
-                                }
-                                
+                                dartClubText
                                 Spacer()
-                                
-                                gameControls
-                                
-                                HStack {
-                                    
-                                    Toggle(isOn: $viewModel.currentGame.isToggledDoubleOut) {
-                                        Text("Double Out")
-                                            .font(.system(size: 18))
-                                    }
-                                    .padding()
-                                    
-                                    Spacer()
-                                }
-                                .frame(width: 200)
-                                
+                                mainContentVStack
                                 Spacer()
-                                
                             }
                             
-                            Spacer()
-
+                        } else {
+                            VStack {
+                                dartClubText
+                                    .padding(.top, 80)
+                                mainContentVStack
+                            }
                         }
                         
                     } else {
-                        
                         VStack {
-                                
-                            Spacer()
-                                
-                            if isDartClubVisible {
-
+                            if geometry.size.height > 550 {
                                 dartClubText
-
                             }
-                                
-                            playerFields
-                                
-                            Spacer()
-                                
-                            if viewModel.currentGame.players.count < 4 {
-                                Button(action: {
-                                    withAnimation {
-                                        viewModel.addPlayer()
-                                        focusedPlayerIndex = viewModel.currentGame.players.count - 1
-                                    }
-                                }) {
-                                    Label("Add a player", systemImage: "person.fill.badge.plus")
-                                        .accessibilityLabel("Add a player")
-                                        .font(.system(size: 20))
-                                        .padding(.top, 20)
-                                }
-                                .buttonStyle(PlainButtonStyle())
-                            }
-                            
-                            Spacer()
-                            
-                            gameControls
-                            
-                            HStack {
-                                
-                                Toggle(isOn: $viewModel.currentGame.isToggledDoubleOut) {
-                                    Text("Double Out")
-                                        .font(.system(size: 18))
-                                }
-                                .padding()
-                                
-                                Spacer()
-                            }
-                            .frame(width: 200)
-                            
-                            Spacer()
-                            
+                            mainContentVStack
                         }
                     }
-                    
-                    Spacer()
-                    
                 }
                 .foregroundColor(.white)
                 .font(.system(size: 20, weight: .bold, design: .default))
                 .navigationBarBackButtonHidden(true)
                 .interactiveDismissDisabled()
+                .navigationDestination(isPresented: $navigateToGame) {
+                    GameView(selectedGame: viewModel.currentGame.gameType, players: viewModel.currentGame.players, viewModel: viewModel)
+                }
             }
-            .navigationDestination(isPresented: $navigateToGame) {
-                GameView(selectedGame: viewModel.currentGame.gameType, players: viewModel.currentGame.players, viewModel: viewModel)
-            }
-            .foregroundColor(.white)
-            .font(.system(size: 20, weight: .bold, design: .default))
-            .navigationBarBackButtonHidden(true)
-            .interactiveDismissDisabled()
+            .preferredColorScheme(.dark)
         }
-        .preferredColorScheme(.dark)
-        }
-    
+    }
+
     var dartClubText: some View {
         VStack {
-            
             Text("Dart   ")
                 .font(Font
                     .custom("FightThis", size: UIDevice.current.userInterfaceIdiom == .pad ? 145 : 84))
-                .shadow(color: Color.red, radius: 15)
-                .foregroundColor(.red)
-                .multilineTextAlignment(.center)
-                .rotationEffect(Angle(degrees: 347))
-                .frame(maxWidth: .infinity)
-                .padding([.trailing], 40)
-            
-            Text("Club   ")
+            .shadow(color: Color.red, radius: 15)
+            .foregroundColor(.red)
+            .multilineTextAlignment(.center)
+            .rotationEffect(Angle(degrees: 347))
+            .padding([.trailing], 40)
+        
+        Text("Club   ")
                 .font(Font
                     .custom("FightThis", size: UIDevice.current.userInterfaceIdiom == .pad ? 145 : 84))
-                .shadow(color: Color.red, radius: 15)
-                .foregroundColor(.red)
-                .multilineTextAlignment(.center)
-                .rotationEffect(Angle(degrees: 347))
-                .frame(maxWidth: .infinity)
-                .padding([.trailing], 40)
-                .padding(.top, UIDevice.current.userInterfaceIdiom == .pad ? 0 : -80)
-
-            
+            .shadow(color: Color.red, radius: 15)
+            .foregroundColor(.red)
+            .multilineTextAlignment(.center)
+            .rotationEffect(Angle(degrees: 347))
+            .padding([.trailing], 40)
+            .padding(.top, UIDevice.current.userInterfaceIdiom == .pad ? 0 : -80)
         }
         .transition(.opacity)
     }
-    
-    var playerFields: some View {
+
+    var mainContentVStack: some View {
+        VStack {
+            Spacer()
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                playerFieldsTwoRows
+            } else {
+                playerFieldsOneRow
+            }
+            if viewModel.currentGame.players.count < 4 {
+                addButton
+            }
+            Spacer()
+            gameControls
+            settingsToggle
+            Spacer()
+        }
+    }
+
+    var playerFieldsOneRow: some View {
         ForEach(0..<viewModel.currentGame.players.count, id: \.self) { index in
-            TextField("Player \(index + 1)", text: $viewModel.currentGame.players[index].name)
-                .padding(.horizontal, 50)
-                .textFieldStyle(.roundedBorder)
-                .frame(width: 350)
-                .multilineTextAlignment(.center)
-                .disableAutocorrection(true)
-                .foregroundColor(.primary)
-                .focused($focusedPlayerIndex, equals: index)
-                .onChange(of: focusedPlayerIndex) {
-                    if focusedPlayerIndex != nil {
-                        withAnimation(.easeInOut) {
-                            isDartClubVisible = false
-                        }
-                    } else {
-                        withAnimation(.easeInOut) {
-                            isDartClubVisible = true
-                        }
-                    }
+            playerTextField(index: index)
+
+        }
+    }
+
+    var playerFieldsTwoRows: some View {
+        VStack {
+
+            HStack {
+                if viewModel.currentGame.players.count > 0 {
+                    playerTextField(index: 0)
+                }
+                if viewModel.currentGame.players.count > 1 {
+                    playerTextField(index: 1)
                 }
             }
+
+            HStack {
+                if viewModel.currentGame.players.count > 2 {
+                    playerTextField(index: 2)
+                }
+                if viewModel.currentGame.players.count > 3 {
+                    playerTextField(index: 3)
+                }
+            }
+        }
+    }
+
+    private func playerTextField(index: Int) -> some View {
+        TextField("Player \(index + 1)", text: $viewModel.currentGame.players[index].name)
+            .padding(.horizontal, 10)
+            .textFieldStyle(.roundedBorder)
+            .frame(width: 250)
+            .multilineTextAlignment(.center)
+            .disableAutocorrection(true)
+            .foregroundColor(.primary)
+            .focused($focusedPlayerIndex, equals: index)
             .font(.title)
     }
-    
+
+    var addButton: some View {
+        Button(action: {
+            withAnimation {
+                viewModel.addPlayer()
+                focusedPlayerIndex = viewModel.currentGame.players.count - 1
+            }
+        }) {
+            Label("Add a player", systemImage: "person.fill.badge.plus")
+                .accessibilityLabel("Add a player")
+                .font(.system(size: 20))
+                .padding(.top, 20)
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+
     var gameControls: some View {
         HStack {
             Button("301") {
-                print("--------------------------------------------")
-                print("BUTTON 301")
-                viewModel.currentGame.gameType = 301
-                viewModel.gameStarted = true
-                navigateToGame = true
+                withAnimation {
+                    viewModel.currentGame.gameType = 301
+                    viewModel.gameStarted = true
+                    navigateToGame = true
+                }
             }
             .disabled(!canStartGame)
             .buttonStyle(.borderedProminent)
             .controlSize(.large)
-            
-            Text("or")
-                .font(.system(size: 20))
-            
-//                            .padding(.bottom, 55)
-            
+
+            Text("or").font(.system(size: 20))
+
             Button("501") {
-                print("--------------------------------------------")
-                print("BUTTON 501")
-                viewModel.currentGame.gameType = 501
-                viewModel.gameStarted = true
-                navigateToGame = true
+                withAnimation {
+                    viewModel.currentGame.gameType = 501
+                    viewModel.gameStarted = true
+                    navigateToGame = true
+                }
             }
             .disabled(!canStartGame)
             .buttonStyle(.borderedProminent)
             .controlSize(.large)
-            
-//////////////////////////////////////////////////////////////////// SETTINGSVIEW /////////////////////////////////////////////////////////////////
-            
-//                        .padding(.bottom, 55)
-
-//                    Button(action: {
-//                        print("--------------------------------------------")
-//                        print("BUTTON SettingsView")
-//                        showSettingsView = true
-//                        }) {
-//                            Image(systemName: "gearshape")
-//                                .accessibilityLabel("Undo")
-//                                .font(.system(size: 20))
-//                        }
-//                        .sheet(isPresented: $showSettingsView) {
-//                            SettingsView()
-//                        }
-//                        .padding(.bottom, 55)
-            
-            
-//////////////////////////////////////////////////////////////////// DEBUG BUTTON /////////////////////////////////////////////////////////////////
-
-//        Button(action: {
-//
-//            print("--------------------------------------------")
-//            print("--------------------------------------------")
-//            print("DEBUG")
-//            print("--------------------------------------------")
-//            print("--------------------------------------------")
-//            print("DOUBLE OUT: \(viewModel.currentGame.isToggledDoubleOut)")
-//            print(" ")
-//            print(" ")
-//
-//        }) {
-//            Image(systemName: "ladybug.circle")
-//                .accessibilityLabel("Undo")
-//                .font(.system(size: 25))
-//        }
-//        .buttonStyle(PlainButtonStyle())
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            
         }
+    }
 
+    var settingsToggle: some View {
+        Toggle(isOn: $viewModel.currentGame.isToggledDoubleOut) {
+            Text("Double Out").font(.system(size: 18))
+        }
+        .padding()
+        .frame(width: 200)
     }
     
     var canStartGame: Bool {
         !viewModel.currentGame.players.isEmpty && viewModel.currentGame.players.allSatisfy { !$0.name.isEmpty }
     }
-
+    
 }
+
 
 
 // ///////////////////////////
