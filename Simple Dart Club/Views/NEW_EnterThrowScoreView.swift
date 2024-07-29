7aa3fb5d574b87a764d32378109930666745baa0
Vnt0n <antoine.chosson@gmail.com>
2024-07-29 13:56:10 +0200

    First New_EnterThrowScore logic

--- Simple Dart Club/Views/NEW_EnterThrowScoreView.swift
+++ Simple Dart Club/Views/NEW_EnterThrowScoreView.swift
@@ -14,74 +14,119 @@
 
 import SwiftUI
 
-struct EnteThrowScoreView_V2: View {
-//    @ObservedObject var viewModel: GameViewModel
+struct EnterThrowScoreView_V2: View {
+    @ObservedObject var viewModel: GameViewModel
     @Environment(\.dismiss) var dismiss
-
     
     let buttonNumbers = [
-            1, 2, 3, 4,
-            5, 6, 7, 8,
-            9, 10, 11, 12,
-            13, 14, 15, 16,
-            17, 18, 19, 20,
-            0, 25
-        ]
+        1, 2, 3, 4,
+        5, 6, 7, 8,
+        9, 10, 11, 12,
+        13, 14, 15, 16,
+        17, 18, 19, 20,
+        0, 25
+    ]
 
-        var body: some View {
-            VStack {
-//                Text("\(viewModel.currentGame.players[viewModel.currentPlayerIndex].name)")
-                Text("TEST")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding(.bottom, 20)
-                
-                // Grid of buttons
-                LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 4), spacing: 15) {
-                    ForEach(buttonNumbers, id: \.self) { number in
-                        Button(action: {
-                            // Action for button press
-                        }) {
-                            Text("\(number)")
-                                .font(.title)
-                                .frame(width: 50, height: 50)
-                                .foregroundColor(.blue)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 8)
-                                        .stroke(Color.blue, lineWidth: 2)
-                                )
-                        }
-                    }
-                }
-                .padding(.horizontal, 40)
-                
-                Spacer().frame(height: 40)
-                
-                // Multiplier buttons
-                HStack(spacing: 20) {
-                    ForEach(1...3, id: \.self) { multiplier in
-                        Button(action: {
-                            // Action for multiplier button press
-                        }) {
-                            Text("x\(multiplier)")
-                                .font(.title)
-                                .frame(width: 50, height: 50)
-                                .foregroundColor(.white)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 8)
-                                        .stroke(Color.white, lineWidth: 2)
-                                )
-                        }
-                    }
-                }
+    var body: some View {
+        VStack {
+            Text("\(viewModel.currentGame.players[viewModel.currentPlayerIndex].name)")
+                .font(.largeTitle)
+                .padding(.bottom, 10)
+            Text("Enter your score")
+                .font(.title)
+                .foregroundColor(.white)
                 .padding(.bottom, 20)
+            
+            // Grid of buttons
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 4), spacing: 15) {
+                ForEach(buttonNumbers, id: \.self) { number in
+                    Button(action: {
+                        // Action for button press
+                    }) {
+                        Text("\(number)")
+                            .font(.title)
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.blue)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.blue, lineWidth: 2)
+                            )
+                    }
+                }
             }
-            .preferredColorScheme(.dark)
-            .edgesIgnoringSafeArea(.all)
+            .padding(.horizontal, 40)
+            
+            Spacer().frame(height: 40)
+            
+            // Multiplier buttons
+            HStack(spacing: 20) {
+                ForEach(1...3, id: \.self) { multiplier in
+                    Button(action: {
+                        // Action for multiplier button press
+                    }) {
+                        Text("x\(multiplier)")
+                            .font(.title)
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.white, lineWidth: 2)
+                            )
+                    }
+                }
+            }
+            .padding(.bottom, 20)
         }
+        .preferredColorScheme(.dark)
+        .edgesIgnoringSafeArea(.all)
+    }
 }
 
 
-#Preview {
-    EnteThrowScoreView_V2()
+
+// ///////////////////////////
+// PREVIEW //////////////////
+
+struct EnterThrowScoreView_V2_Previews: PreviewProvider {
+    static var previews: some View {
+        let gameType = 180
+        let viewModel = GameViewModel(gameType: gameType)
+
+        while viewModel.currentGame.players.count < 4 {
+            viewModel.addPlayer()
+        }
+
+        viewModel.currentGame.players[0].name = "Alice"
+        viewModel.currentGame.players[1].name = "Bob"
+        viewModel.currentGame.players[2].name = "Charlie"
+        viewModel.currentGame.players[3].name = "Dana"
+
+        viewModel.currentGame.isToggledDoubleOut = true
+
+        return Group {
+            EnterThrowScoreView_V2(viewModel: viewModel)
+                .previewDisplayName("English")
+                .environment(\.locale, Locale(identifier: "en"))
+
+            EnterThrowScoreView_V2(viewModel: viewModel)
+                .previewDisplayName("French")
+                .environment(\.locale, Locale(identifier: "fr"))
+
+            EnterThrowScoreView_V2(viewModel: viewModel)
+                .previewDisplayName("Spanish")
+                .environment(\.locale, Locale(identifier: "es"))
+
+            EnterThrowScoreView_V2(viewModel: viewModel)
+                .previewDisplayName("German")
+                .environment(\.locale, Locale(identifier: "de"))
+
+            EnterThrowScoreView_V2(viewModel: viewModel)
+                .previewDisplayName("Chinese")
+                .environment(\.locale, Locale(identifier: "zh-Hans"))
+
+            EnterThrowScoreView_V2(viewModel: viewModel)
+                .previewDisplayName("Japanese")
+                .environment(\.locale, Locale(identifier: "ja"))
+        }
+    }
 }
